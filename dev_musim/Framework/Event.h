258dce40e0b000a11913f1ad728f8335a6b2173b
Alvaro Taboada <alvaro.taboada@iteda.cnea.gov.ar>
2021-06-29 11:29:00 -0300

    remove ParticleFileReader (now in IO). add AddPhoton methods

--- dev_musim/Framework/Event.h
+++ dev_musim/Framework/Event.h
@@ -6,38 +6,46 @@
 #include <string>
 
 /**
-	MuSim Event Class
-	
-	This class is intended to store methods and variables
-	that will be handled during the simulation / reconstruction
-	processes. 
+  MuSim Event Class
 
-	todo: Description about "event" concept.
+  This class is intended to store methods and variables
+  that will be handled during the simulation / reconstruction
+  processes. 
 
-	author: Alvaro Taboada
-	date: 16 June 2021
+  todo: Description about "event" concept.
 
-	$Id:$
+  author: Alvaro Taboada
+  date: 16 June 2021
+
+  $Id:$
 */
 
 
 class Event 
 {
-	public:
-		Event() { fParticles.clear(); }
-		static Event ReadParticleFile(const std::string &fileName);
+  public:
+    
+    Event();
 
-		typedef std::vector<Particle> ParticleVector;
-		const ParticleVector& GetParticleVector() const { return fParticles; }
-		void InsertParticle(const Particle& particle) { fParticles.push_back(particle); }
-		
-		unsigned int GetTotalNumberOfParticles() const { return fTotalNumberOfParticles; }
-		void SetTotalNumberOfParticles(const unsigned int n) { fTotalNumberOfParticles = n; }
-		
-	private:
+    typedef std::vector<Particle> ParticleVector;
+    const ParticleVector& GetParticleVector() const { return fParticles; }
+    void InsertParticle(const Particle& particle) { fParticles.push_back(particle); }
 
-		ParticleVector fParticles;
-		int fTotalNumberOfParticles = 0;
+    //mover a SimData
+    unsigned int GetTotalNumberOfParticles() const { return fTotalNumberOfParticles; }
+    void SetTotalNumberOfParticles(const unsigned int n) { fTotalNumberOfParticles = n; }
+
+    // por ahora aqui. eventualmente sera un metodo de la clase SimData 
+    // y tomara como entrada el id del SiPM donde los fotones fueron detectados
+    void AddPhotonTime(const double peTime);
+    std::vector<double> GetPhotonTime() { return fPETimes; }
+
+  private:
+
+    ParticleVector fParticles;
+    int fTotalNumberOfParticles = 0;
+    std::vector<double> fPETimes;
+
 };
 
 
