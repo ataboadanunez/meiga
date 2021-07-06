1eb7ec0ddea90875288af428c9892f6bf43b885f
Alvaro Taboada <alvaro.taboada@iteda.cnea.gov.ar>
2021-07-05 23:35:52 -0300

    methods to set particle mass and momentum at the constructor

--- dev_musim/Utilities/Particle.cc
+++ dev_musim/Utilities/Particle.cc
@@ -1,8 +1,11 @@
 #include "Particle.h"
+#include "PhysicsConstants.h"
+#include <sstream>
+#include <cmath>
 
 Particle::Particle(const int id, const double px,
                    const double py, const double pz,
-                   const double x, const double y, const double z):
+                   const double x, const double y, const double z) : 
                   fId(id),
                   fPx(px),
                   fPy(py),
@@ -11,6 +14,120 @@ Particle::Particle(const int id, const double px,
                   fY(y),
                   fZ(z)
                    { }
+
+Particle::Particle(const int id, const std::vector<double>& position, const std::vector<double>& momentum) :
+  fPosition(position),
+  fDirection(momentum),
+  fId(id)
+  {
+    SetMomentum(momentum);
+    SetMass(id);
+  }
+
+
+void
+Particle::SetMass(const int id)
+{
+  if (gParticleMassMap.empty())
+    InitParticleMassMap();
+
+  //const auto index = gParticleMassMap.find(id);
+  //fMass = (index != gParticleMassMap.end()) ? index->second : 0. ;
+  //return (index != gParticleMassMap.end()) ? index->second : 0.;
+  fMass = gParticleMassMap[id];
+}
+
+void
+Particle::InsertParticleMass(const Particle::Type id, const double mass)
+  {
+    const auto ite = gParticleMassMap.insert(std::make_pair(id, mass));
+    if (!ite.second) {
+      std::ostringstream err;
+      err << "Particle with Corsika id " << id << " already inserted.";
+      throw err;
+    }
+  }
+
+void
+Particle::InitParticleMassMap()
+{ 
+  // Particle ID is the CORSIKA ID
+  gParticleMassMap.emplace(1, 0);
+  gParticleMassMap.emplace(2,  kElectronMass);
+  gParticleMassMap.emplace(3,  kElectronMass);
+  gParticleMassMap.emplace(5, kMuonMass);
+  gParticleMassMap.emplace(6, kMuonMass);
+  gParticleMassMap.emplace(7, kPiZeroMass);
+  gParticleMassMap.emplace(8, kPiChargedMass);
+  gParticleMassMap.emplace(9, kPiChargedMass);
+  gParticleMassMap.emplace(10, kKaonZeroMass);
+  gParticleMassMap.emplace(11, kKaonChargedMass);
+  gParticleMassMap.emplace(12, kKaonChargedMass);
+  gParticleMassMap.emplace(13, kNeutronMass);
+  gParticleMassMap.emplace(14, kProtonMass);
+  gParticleMassMap.emplace(15, kProtonMass);
+  gParticleMassMap.emplace(16, kKaonZeroMass);
+  gParticleMassMap.emplace(17, kEtaMass);
+  gParticleMassMap.emplace(18, kLambdaMass);
+  gParticleMassMap.emplace(19, kSigmaPlusMass);
+  gParticleMassMap.emplace(20, kSigmaZeroMass);
+  gParticleMassMap.emplace(21, kSigmaMinusMass);
+  gParticleMassMap.emplace(22, kXiZeroMass);
+  gParticleMassMap.emplace(23, kXiMinusMass);
+  gParticleMassMap.emplace(24, kOmegaMinusMass);
+  gParticleMassMap.emplace(25, kNeutronMass);
+  gParticleMassMap.emplace(26, kLambdaMass);
+  gParticleMassMap.emplace(27, kSigmaPlusMass);
+  gParticleMassMap.emplace(28, kSigmaZeroMass);
+  gParticleMassMap.emplace(29, kSigmaMinusMass);
+  gParticleMassMap.emplace(30, kXiZeroMass);
+  gParticleMassMap.emplace(31, kXiMinusMass);
+  gParticleMassMap.emplace(32, kOmegaMinusMass);
+
+  gParticleMassMap.emplace(49, kPhiMass);
+
+  gParticleMassMap.emplace(50, kOmegaMesonMass);
+  gParticleMassMap.emplace(51, kRhoZeroMass);
+  gParticleMassMap.emplace(52, kRhoChargedMass);
+  gParticleMassMap.emplace(53, kRhoChargedMass);
+  gParticleMassMap.emplace(54, kDeltaMass);
+  gParticleMassMap.emplace(55, kDeltaMass);
+  gParticleMassMap.emplace(56, kDeltaMass);
+  gParticleMassMap.emplace(57, kDeltaMass);
+  gParticleMassMap.emplace(58, kDeltaMass);
+  gParticleMassMap.emplace(59, kDeltaMass);
+  gParticleMassMap.emplace(60, kDeltaMass);
+  gParticleMassMap.emplace(61, kDeltaMass);
+  gParticleMassMap.emplace(62, kKaonStarZeroMass);
+  gParticleMassMap.emplace(63, kKaonStarChargedMass);
+  gParticleMassMap.emplace(64, kKaonStarChargedMass);
+  gParticleMassMap.emplace(65, kKaonStarZeroMass);
+  gParticleMassMap.emplace(66, 0.);
+  gParticleMassMap.emplace(67, 0.);
+  gParticleMassMap.emplace(68, 0.);
+  gParticleMassMap.emplace(69, 0.);
+
+  gParticleMassMap.emplace(71, kEtaMass);
+  gParticleMassMap.emplace(72, kEtaMass);
+  gParticleMassMap.emplace(73, kEtaMass);
+  gParticleMassMap.emplace(74, kEtaMass);
+
+  gParticleMassMap.emplace(95, kMuonMass);
+  gParticleMassMap.emplace(96, kMuonMass);
+  
+}
+
+void
+Particle::SetMomentum(const std::vector<double>& momentum)
+{
+  const double px = momentum[0];
+  const double py = momentum[1];
+  const double pz = momentum[2];
+  const double mass = GetMass();
+  std::cout << "[DEBUG] FRAMEWORK " << mass << std::endl; 
+  fMomentum = sqrt(px*px + py*py + pz*pz);
+  fKineticEnergy = sqrt(fMomentum*fMomentum + mass*mass) - mass;
+}
 
 int
 Particle::NucleusCode(const unsigned int charge,
--- dev_musim/Utilities/Particle.h
+++ dev_musim/Utilities/Particle.h
@@ -3,83 +3,108 @@
 
 #include <iostream>
 #include <string>
+#include <map>
+#include <vector>
 
 // start using namespaces?
 
 class Particle {
 
-public:
+	public:
 
-	// useful definitions for nucleus code calculation
-	static const int kNucleusBase        = 1000000000;
-  static const int kNucleusMax         = 1000100300;
-  static const int kAtomicNumberFactor = 1;
-  static const int kAtomicNumberMask   = 1000;
-  static const int kChargeFactor       = 1000;
-  static const int kChargeMask         = 1000000;
 
-  // PDG encoding
-	enum Type {
+		// useful definitions for nucleus code calculation
+		static const int kNucleusBase        = 1000000000;
+		static const int kNucleusMax         = 1000100300;
+		static const int kAtomicNumberFactor = 1;
+		static const int kAtomicNumberMask   = 1000;
+		static const int kChargeFactor       = 1000;
+		static const int kChargeMask         = 1000000;
+
+		// PDG encoding
+		enum Type {
 			eUndefined = 0,
-      eElectron = 11, ePositron = -11,
-      eNuElectron = 12, eAntiNuElectron = -12,
-      eMuon = 13, eAntiMuon = -13,
-      eNuMuon = 14, eAntiNuMuon = -14,
-      eTau = 15, eAntiTau = -15,
-      eNuTau = 16, eAntiNuTau = -16,
-      ePhoton = 22,
-      // PDG codes 81-100 are reserved for special/pseudo particles
-      eDecayedMuon = 96, eDecayedAntiMuon = 95,
-      ePiZero = 111,
-      eRhoZero = 113,
-      ePiPlus = 211, ePiMinus = -211,
-      eRhoPlus = 213, eRhoMinus = -213,
-      eEta = 221,
-      eOmegaMeson = 223,
-      eKaon0L = 130, eKaon0S = 310,
-      eKaonStarZero = 313, eAntiKaonStarZero = -313,
-      eKaonPlus = 321, eKaonMinus = -321,
-      eKaonStarPlus = 323, eAntiKaonStarMinus = -323,
-      ePhi = 333,
-      eDeltaMinus = 1114, eAntiDeltaPlus = -1114,
-      eNeutron = 2112, eAntiNeutron = -2112,
-      eDeltaZero = 2114, eAntiDeltaZero = -2114,
-      eProton = 2212, eAntiProton = -2212,
-      eDeltaPlus = 2214, eAntiDeltaMinus = -2214,
-      eDeltaPlusPlus = 2224, eAntiDeltaMinusMinus = -2224,
-      eLambda = 3122, eAntiLambda = -3122,
-      eSigmaPlus = 3222, eSigmaZero = 3212, eSigmaMinus = 3112,
-      eAntiSigmaPlus = -3222, eAntiSigmaZero = -3212, eAntiSigmaMinus = -3112,
-      eXiZero = 3322, eXiMinus = 3312,
-      eAntiXiZero = -3322, eAntiXiMinus = -3312,
-      eOmegaMinus = 3332, eAntiOmegaMinus = -3332,
-      eLambdac = 4122,
-      eIron = 1000026056
-  };
+			eElectron = 11, ePositron = -11,
+			eNuElectron = 12, eAntiNuElectron = -12,
+			eMuon = 13, eAntiMuon = -13,
+			eNuMuon = 14, eAntiNuMuon = -14,
+			eTau = 15, eAntiTau = -15,
+			eNuTau = 16, eAntiNuTau = -16,
+			ePhoton = 22,
+			// PDG codes 81-100 are reserved for special/pseudo particles
+			eDecayedMuon = 96, eDecayedAntiMuon = 95,
+			ePiZero = 111,
+			eRhoZero = 113,
+			ePiPlus = 211, ePiMinus = -211,
+			eRhoPlus = 213, eRhoMinus = -213,
+			eEta = 221,
+			eOmegaMeson = 223,
+			eKaon0L = 130, eKaon0S = 310,
+			eKaonStarZero = 313, eAntiKaonStarZero = -313,
+			eKaonPlus = 321, eKaonMinus = -321,
+			eKaonStarPlus = 323, eAntiKaonStarMinus = -323,
+			ePhi = 333,
+			eDeltaMinus = 1114, eAntiDeltaPlus = -1114,
+			eNeutron = 2112, eAntiNeutron = -2112,
+			eDeltaZero = 2114, eAntiDeltaZero = -2114,
+			eProton = 2212, eAntiProton = -2212,
+			eDeltaPlus = 2214, eAntiDeltaMinus = -2214,
+			eDeltaPlusPlus = 2224, eAntiDeltaMinusMinus = -2224,
+			eLambda = 3122, eAntiLambda = -3122,
+			eSigmaPlus = 3222, eSigmaZero = 3212, eSigmaMinus = 3112,
+			eAntiSigmaPlus = -3222, eAntiSigmaZero = -3212, eAntiSigmaMinus = -3112,
+			eXiZero = 3322, eXiMinus = 3312,
+			eAntiXiZero = -3322, eAntiXiMinus = -3312,
+			eOmegaMinus = 3332, eAntiOmegaMinus = -3332,
+			eLambdac = 4122,
+			eIron = 1000026056
+		};
 
-  /// Constructor using momentum
-  Particle() {;}
-  Particle(const int id, const double px, const double py, const double pz, const double x, const double y, const double z);
-  /*
-  Particle(const int type, 
-  				 const Source& source, const Point& position,
-           const Vector& momentum, const TimeInterval& time,
-           const double weight);
-	*/
+		/// Constructor using momentum
+		Particle() { ; }
+		Particle(const int id, const double px, const double py, const double pz, const double x, const double y, const double z);
+		// here momentum is the momentum direction (px, py, pz) as it comes from the input file
+		Particle(const int id, const std::vector<double>& position, const std::vector<double>& momentum);
 
-  /// Calculate particle code for a nucleus given (A, Z)
-  static int NucleusCode(const unsigned int theCharge, const unsigned int theAtomicNumber);
-  ////Getters
-  int GetParticleId() const { return fId; }
-  double GetParticleMomentumX() const { return fPx; }
-  double GetParticleMomentumY() const { return fPy; }
-  double GetParticleMomentumZ() const { return fPz; }
-  double GetParticlePositionX()  const { return fX; }
-  double GetParticlePositionY()  const { return fY; }
-  double GetParticlePositionZ()  const { return fY; }
-  private:
-  int fId;
-  double fPx, fPy, fPz, fX, fY, fZ;
+		int GetParticleId() const { return fId; }
+		/// Calculate particle code for a nucleus given (A, Z)
+		static int NucleusCode(const unsigned int theCharge, const unsigned int theAtomicNumber);
+
+		// particle momentum
+		const std::vector<double>& GetParticleDirection() const { return fDirection; }
+		void SetMomentum(const std::vector<double>& momentum);
+		double GetMomentum() const { return fMomentum; }
+		
+		// particle mass
+		void InsertParticleMass(const Particle::Type id, const double mass);
+		void InitParticleMassMap();
+		void SetMass(const int id);
+		double GetMass() const { return fMass; }	
+		
+		// particle energy
+		double GetKineticEnergy() const { return fKineticEnergy; }
+
+		double GetParticleMomentumX() const { return fPx; }
+		double GetParticleMomentumY() const { return fPy; }
+		double GetParticleMomentumZ() const { return fPz; }
+		double GetParticlePositionX()  const { return fX; }
+		double GetParticlePositionY()  const { return fY; }
+		double GetParticlePositionZ()  const { return fY; }
+
+
+
+	private:
+ 		
+ 		int fId;
+ 		double fPx, fPy, fPz, fX, fY, fZ;
+ 		double fMass = 0;
+ 		double fMomentum = 0;
+ 		double fKineticEnergy = 0;
+
+ 		std::vector<double> fPosition;
+  	std::vector<double> fDirection;
+  	std::map<int, double> gParticleMassMap;
+		// particle properties for mass
 };
 
 #endif 
