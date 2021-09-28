b821ecf08b4725c7a4dece2869e3fc28bb5e05e6
Alvaro Taboada <alvaro.taboada@iteda.cnea.gov.ar>
2021-09-28 16:57:59 -0300

    ignore build install

--- dev_meiga/IO/DataWriter.h
+++
@@ -1,9 +0,0 @@
-#ifndef DataWriter_h_1
-#define DataWriter_h_1
-
-#include "Event.h"
-#include <string>
-
-// start with AsciiWriter function
-
-#endif
--- dev_meiga/cmake/Framework/Detector/CMakeFiles/meiga_framework_detector.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for meiga_framework_detector.
-# This may be replaced when dependencies are built.
