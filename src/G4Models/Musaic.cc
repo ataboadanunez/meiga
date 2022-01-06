8976c1bfec6a876869134ed41fbf420934fe0106
Alvaro Taboada <alvaro.taboada.nunez@gmail.com>
2022-01-06 11:27:32 -0300

    naming, comments, cleanup

--- src/G4Models/Musaic.cc
+++ src/G4Models/Musaic.cc
@@ -27,6 +27,7 @@ Musaic::BuildDetector(G4LogicalVolume* logMother, Detector& detector, Event& the
   auto  detectorPos = Geometry::ToG4Vector(pos, 1.);
 	int detectorId = detector.GetId();
 	int nBars = detector.GetNBars();
+	
 	// detector properties
 	fCasingThickness = detector.GetCasingThickness() * mm;
 	// scintillator bar properties
@@ -39,50 +40,43 @@ Musaic::BuildDetector(G4LogicalVolume* logMother, Detector& detector, Event& the
 	fCladdingThickness = detector.GetCladdingThickness() * mm;
 	fFiberRadius = detector.GetFiberRadius() * mm;
 
-	//detector.MakeOptDevice(OptDevice::eSiPM);
+	// Optical device properties
 	OptDevice sipm = detector.GetOptDevice(OptDevice::eSiPM);
-	std::cout << "[DEBUG] G4Models::Musaic: Building detector Musaic with " << sipm.GetName() << ". " << std::endl;
 	fSiPMSizeZ = sipm.GetThickness() * mm;
 	fSiPMSizeX = sipm.GetLength() * mm;
 	fSiPMSizeY = sipm.GetWidth() * mm;
-	std::cout << "[DEBUG] Number of bars = " << nBars << std::endl;
-	std::cout << "[DEBUG] Bar Length / mm = " << fBarLength  << std::endl;
-	std::cout << "[DEBUG] Bar Width / mm = " << fBarWidth  << std::endl;
-	std::cout << "[DEBUG] Bar Thickness / mm = " << fBarThickness  << std::endl;
-	std::cout << "Optical Device dimensions = (" << fSiPMSizeX << ", " << fSiPMSizeY << ", " << fSiPMSizeZ << ") / mm" << std::endl;
-
-	fCasingSizeX = fBarLength/2 + fCoatingThickness + fCasingThickness;
-	fCasingSizeY = fBarWidth/2 + fCoatingThickness;
-	fCasingSizeZ = fBarThickness/2 + fCoatingThickness;
 
 	// define a enclosure volume that contains the components of the detector
-	// this is just to ease the detector construction...not a proper volume
-	ostringstream namedetector;
-	namedetector.str("");
-	namedetector << "Casing" << '_' << detectorId;
-	const double x = 0.5*fBarLength;
-	const double y = 0.5*fBarLength;
-	const double z = 0.5*fBarThickness * 2; // 2 = number of panels
-	const double t = fCasingThickness;
-	std::cout << "[DEBUG] G4Models::Musaic: up to here all good! " << std::endl;
+	fCasingSizeX = 0.5*fBarLength + fCasingThickness;
+	fCasingSizeY = 0.5*fBarLength + fCasingThickness;
+	fCasingSizeZ = 0.5*fBarThickness * 2 + fCasingThickness; // (x2 = number of panels)
+
+	string namedetector = "Musaic";
+	string newname; 
+	newname = namedetector + '_' + to_string(detectorId) + '/';
+	
+	std::cout << "[DEBUG] G4Models::Musaic: Building detector " << namedetector << " with " << sipm.GetName() << ". " << std::endl;
+	
+
+	/****************************
+		
+		Geant4 Volume construction
+	
+	****************************/ 
+
 	// Casing
-	solidCasing = new G4Box("Casing", x+t, y+t, z+t);
+	solidCasing = new G4Box("Casing", fCasingSizeX, fCasingSizeY, fCasingSizeZ);
 	logicCasing = new G4LogicalVolume(solidCasing, Materials().Air, "Casing", 0, 0, 0);
 	physCasing  = new G4PVPlacement(nullptr, detectorPos, logicCasing, "Casing", logMother, false, detectorId, fCheckOverlaps);
-	std::cout << "[DEBUG] G4Models::Musaic: Building Casing! " << std::endl;
 	// Bars: Coating + Scintillator bar
 	solidCoating  	= new G4Box("BarCoating", fCoatingThickness + fBarLength/2, fCoatingThickness + fBarWidth/2, fCoatingThickness + fBarThickness/2);
 	solidScinBar   	= new G4Box("BarScin", fBarLength/2, fBarWidth/2, fBarThickness/2);
-	std::cout << "[DEBUG] G4Models::Musaic: Building Bars! " << std::endl;
 	// Fiber: Cladding (external & internal) + fiber core
 	solidClad2  = new G4Tubs("Clad2", 0, fFiberRadius, fBarLength/2-2*fCoatingThickness-fSiPMSizeZ, 0, 360*deg);
 	solidClad1  = new G4Tubs("Clad1", 0, fFiberRadius - fCladdingThickness, fBarLength/2-2*fCoatingThickness-fSiPMSizeZ, 0, 360*deg);
 	solidFiber  = new G4Tubs("Fiber", 0, fFiberRadius - 2*fCladdingThickness, fBarLength/2-2*fCoatingThickness-fSiPMSizeZ, 0, 360*deg);
-	std::cout << "[DEBUG] G4Models::Musaic: Building Fibers! " << std::endl;
 	// SiPM
 	solidSiPM   = new G4Box("SiPM", fSiPMSizeX, fSiPMSizeY, fSiPMSizeZ);
-	std::cout << "[DEBUG] G4Models::Musaic: Building SiPM! " << std::endl;
-	
 	new G4LogicalSkinSurface("BarCoating", logicCoating, Materials().ScinOptSurf);
 
 	// useful rotation matrix for bars orientation
@@ -96,27 +90,15 @@ Musaic::BuildDetector(G4LogicalVolume* logMother, Detector& detector, Event& the
 	G4VisAttributes blue(G4Colour::Blue());
 	G4VisAttributes black(G4Colour::Black());
 
-	/****
-		coordinates and orientation of detector components
-		bar length 		along x-axis
-		bar width 		along y-axis
-		bar thickness along z-axis
-
-		in the (MxN) = (2x2) configuration, 2 bars will be placed
-		along the y axis (M) and 2 along the z axis (N). therefore
-		bars should be created within the loop and placed accordingly
-
-		position of top and bottom bars are set wrt detector origin
-
-	*/
+	// definitions for fibers and SiPM placements
 
 	G4double fPosTopX = 0;
-	G4double fPosTopY = fCasingSizeY;
-	G4double fPosTopZ = fCasingSizeZ;
-
-	G4double fPosBotX = fCasingSizeY;
+	G4double fPosTopY = 0.5*fBarWidth + fCoatingThickness;
+	G4double fPosTopZ = 0.5*fBarThickness + fCoatingThickness;
+	// bottom panel is rotated in the XY axis
+	G4double fPosBotX = fPosTopY;
 	G4double fPosBotY = 0;
-	G4double fPosBotZ = -fCasingSizeZ;
+	G4double fPosBotZ = -fPosTopZ;
 	// place fiber at the top of the bar (x-axis)
 	// place SiPM at the end of the bar (z-axis)
 	G4double fFiberTopPosX = fBarLength/2 - 2*fCoatingThickness;
@@ -126,29 +108,22 @@ Musaic::BuildDetector(G4LogicalVolume* logMother, Detector& detector, Event& the
 	// bars of the top panel
 	for (G4int i=0; i<nBars; ++i) {
 
-		/* 
-			update bar placement for nBars>2...
-			maybe create consecutive bars starting form a given point, i.e.,
-			barPos = MoulePosition - nBars * barLength (?) or smth like this...
-		*/
-
+		// definitions for bar placement
 		unsigned int barId = i;
 		G4int inc = pow(-1,i);//(1+2*i);
 		G4double yp = inc*fPosTopY;
-
-		string nameCoating = "BarCoating_"+to_string(barId);
-		string nameScinBar = "BarScin_"+to_string(barId);
-		string nameClad2 = "FiberClad2_"+to_string(barId);
-		string nameClad1 = "FiberClad1_"+to_string(barId);
-		string nameFiber = "Fiber_"+to_string(barId);
-		string nameSiPM = "SiPM_"+to_string(barId);
-
+		
+		// naming
+		G4String nameCoating = "BarCoating_"+to_string(barId);
+		G4String nameScinBar = "BarScin_"+to_string(barId);
+		G4String nameClad2 = "FiberClad2_"+to_string(barId);
+		G4String nameClad1 = "FiberClad1_"+to_string(barId);
+		G4String nameFiber = "Fiber_"+to_string(barId);
+		G4String nameSiPM = "SiPM_"+to_string(barId);
 		// register SiPM in the detector class
 		detector.MakeOptDevice(barId, OptDevice::eSiPM);
-		std::cout << "[DEBUG] Registering SiPM in Detector = " << detector.GetId() << std::endl;
-		// OptDevice& currSipm = detector.GetOptDevice(barId);
-		// currSipm.SetType(OptDevice::eSiPM);
 
+		// logical volumes
 		logicCoating = new G4LogicalVolume(solidCoating, Materials().ScinCoating, nameCoating, 0, 0, 0);
 		logicScinBar  = new G4LogicalVolume(solidScinBar, Materials().ScinPlastic, nameScinBar, 0, 0, 0);
 		logicClad2 = new G4LogicalVolume(solidClad2, Materials().FPethylene, nameClad2, 0, 0, 0);
@@ -160,7 +135,7 @@ Musaic::BuildDetector(G4LogicalVolume* logMother, Detector& detector, Event& the
 		logicSiPM = new G4LogicalVolume(solidSiPM, Materials().Pyrex, nameSiPM, 0, 0, 0);
 		logicSiPM->SetVisAttributes(blue); 
 
-		// top panel upper direction
+		// physical volumes
 		physCoating  = new G4PVPlacement(nullptr, G4ThreeVector(fPosTopX, yp, fPosTopZ), logicCoating, 
 			nameCoating, logicCasing, false, barId, fCheckOverlaps);
 		physScinBar   = new G4PVPlacement(nullptr, G4ThreeVector(), logicScinBar, 
@@ -174,8 +149,8 @@ Musaic::BuildDetector(G4LogicalVolume* logMother, Detector& detector, Event& the
 		physSiPM  = new G4PVPlacement(rotationFiber, G4ThreeVector(fFiberTopPosX, 0, fFiberTopPosZ), logicSiPM, 
 			nameSiPM, logicScinBar, false, barId, fCheckOverlaps);
 
-		// registration of SiPM
-		G4MOptDeviceAction* const SiPMTopSD = new G4MOptDeviceAction("/Musaic/" + namedetector.str() + "/" + nameSiPM, detectorId, barId, theEvent);
+		// registration of SiPM as a Sensitive Volume
+		G4MOptDeviceAction* const SiPMTopSD = new G4MOptDeviceAction(newname + nameSiPM, detectorId, barId, theEvent);
 		sdMan->AddNewDetector(SiPMTopSD);
 		logicSiPM->SetSensitiveDetector(SiPMTopSD);
 
@@ -186,20 +161,22 @@ Musaic::BuildDetector(G4LogicalVolume* logMother, Detector& detector, Event& the
 	// bars of the bottom panel
 	for (G4int i=nBars; i<2*nBars; ++i) {
 
+		// definitions for bar placement
 		unsigned int barId = i;
 		G4int inc = pow(-1,i);
 		G4double xp = inc*fPosBotX;
-
-		string nameCoating = "BarCoating_"+to_string(barId);
-		string nameScinBar = "BarScin_"+to_string(barId);
-		string nameClad2 = "FiberClad2_"+to_string(barId);
-		string nameClad1 = "FiberClad1_"+to_string(barId);
-		string nameFiber = "Fiber_"+to_string(barId);
-		string nameSiPM = "SiPM_"+to_string(barId);
-
+		
+		// naming
+		G4String nameCoating = "BarCoating_"+to_string(barId);
+		G4String nameScinBar = "BarScin_"+to_string(barId);
+		G4String nameClad2 = "FiberClad2_"+to_string(barId);
+		G4String nameClad1 = "FiberClad1_"+to_string(barId);
+		G4String nameFiber = "Fiber_"+to_string(barId);
+		G4String nameSiPM = "SiPM_"+to_string(barId);
 		// register SiPM in the detector class
 		detector.MakeOptDevice(barId, OptDevice::eSiPM);
-
+		
+		// logical volumes
 		logicCoating = new G4LogicalVolume(solidCoating, Materials().ScinCoating, nameCoating, 0, 0, 0);
 		logicScinBar  = new G4LogicalVolume(solidScinBar, Materials().ScinPlastic, nameScinBar, 0, 0, 0);
 		logicClad2 = new G4LogicalVolume(solidClad2, Materials().FPethylene, nameClad2, 0, 0, 0);
@@ -210,8 +187,8 @@ Musaic::BuildDetector(G4LogicalVolume* logMother, Detector& detector, Event& the
 		logicFiber->SetVisAttributes(green); 
 		logicSiPM = new G4LogicalVolume(solidSiPM, Materials().Pyrex, nameSiPM, 0, 0, 0);
 		logicSiPM->SetVisAttributes(blue);
-
-		// upper direction
+		
+		// physical volumes
 		physCoating  = new G4PVPlacement(rotationBot, G4ThreeVector(xp, fPosBotY, fPosBotZ), logicCoating, 
 			nameCoating, logicCasing, false, barId, fCheckOverlaps);
 		physScinBar   = new G4PVPlacement(nullptr, G4ThreeVector(0, 0, 0), logicScinBar,
@@ -225,8 +202,8 @@ Musaic::BuildDetector(G4LogicalVolume* logMother, Detector& detector, Event& the
 		physSiPM  = new G4PVPlacement(rotationFiber, G4ThreeVector(fFiberTopPosX, 0, fFiberBotPosZ), logicSiPM, 
 		nameSiPM, logicScinBar, false, barId, fCheckOverlaps);
 
-		// registration of SiPM
-		G4MOptDeviceAction* const SiPMBotSD = new G4MOptDeviceAction("/Musaic/" + namedetector.str() + "/" + nameSiPM, detectorId, barId, theEvent);
+		// registration of SiPM as a Sensitive Volume
+		G4MOptDeviceAction* const SiPMBotSD = new G4MOptDeviceAction(newname + nameSiPM, detectorId, barId, theEvent);
 		sdMan->AddNewDetector(SiPMBotSD);
 		logicSiPM->SetSensitiveDetector(SiPMBotSD);
 	
