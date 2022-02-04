2fd6126473059ad4a8629b01148c5dce50b7c8d8
Alvaro Taboada <alvaro.taboada.nunez@gmail.com>
2022-02-04 17:29:35 -0300

    stod

--- src/Framework/ConfigManager/ConfigManager.cc
+++ src/Framework/ConfigManager/ConfigManager.cc
@@ -1,12 +1,29 @@
 #include "ConfigManager.h"
 #include "G4UnitsTable.hh"
+#include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string/trim.hpp>
+#include <map>
+#include <string>
 
 using boost::property_tree::ptree;
 using namespace std;
 
+static map<string, enum Detector::DetectorType> conversion;
+
+static void initConversionMap() {
+	static bool isInitialized = false;
+	if (isInitialized) return;
+	isInitialized = true;
+	conversion.clear();
+	conversion["eUnknown"] = Detector::eUnknown;
+	conversion["eMusaic"]  = Detector::eMusaic;
+	conversion["eMudulus"] = Detector::eMudulus;
+}
+
 void
 ConfigManager::ReadDetectorList(string fDetectorList, Event& theEvent)
 {
+	initConversionMap();
 	cout << "ConfigManager::ReadDetectorList: Reading Detector Configuration from file " << fDetectorList << endl;
 	//Detector& detector = theEvent.GetDetector();
 	int nDetectors = 0;
@@ -29,8 +46,9 @@ ConfigManager::ReadDetectorList(string fDetectorList, Event& theEvent)
 		detPosition.clear();
 
 		// create and get detector
-		if (!theEvent.HasDetector(detId))
+		if (!theEvent.HasDetector(detId)) {
 			theEvent.MakeDetector(detId);
+		}
 		
 		Detector& detector = theEvent.GetDetector(detId);
 
@@ -39,26 +57,37 @@ ConfigManager::ReadDetectorList(string fDetectorList, Event& theEvent)
 		for (const auto &v : subtree.get_child("")) {
 			string label = v.first;
 			if ( label != "<xmlattr>" ) {
+				string value = v.second.data();
+				boost::algorithm::trim(value);
 
 				if (label == "type") {
 					std::cout << " reading detector type " << std::endl;
-					auto detType = (Detector::DetectorType)stoi(v.second.data());
-					detector.SetType(detType);
+					try {
+						cout << value << endl;
+						auto detType = conversion[value];
+						detector.SetType(detType);
+					}
+					catch (out_of_range &e) {
+						cerr << "Detector type " << value << " does not exist!" << endl;
+						detector.SetType(Detector::eUnknown);
+					}
 				}	
 				else if (label == "barsInPanel") {
-					int nBarsInPanel = stoi(v.second.data());
+					int nBarsInPanel = stoi(value);
 					detector.SetNBars(nBarsInPanel);
 				}
 				else if (label == "barLength") {
-					double barLength = stoi(v.second.data());
+					cout << value << endl;
+					double barLength = stod(value);
 					string unit = v.second.get<string>("<xmlattr>.unit");
 					double unitG4 = G4UnitDefinition::GetValueOf(unit);
 					detector.SetBarLength(barLength*unitG4);
 				}
 				else {
-					double value = stod(v.second.data());
+					cout << value << endl;
+					double dValue = stod(value);
 					string unit = v.second.get<string>("<xmlattr>.unit");
-					detPosition.push_back(value*G4UnitDefinition::GetValueOf(unit));
+					detPosition.push_back(dValue*G4UnitDefinition::GetValueOf(unit));
 				}
 			}
 		}
