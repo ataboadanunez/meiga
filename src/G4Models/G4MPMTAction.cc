63881dded24e59d3c704a0bf0a95bcc65894d1a8
Alvaro Taboada <alvaro.taboada.nunez@gmail.com>
2022-02-09 18:03:50 -0300

    adding PMTAction class

---
+++ src/G4Models/G4MPMTAction.cc
@@ -0,0 +1,61 @@
+#include "G4MPMTAction.h"
+#include "SimData.h"
+#include "DetectorSimData.h"
+#include "SiPMSimData.h"
+#include "OptDevice.h"
+
+#include <G4Step.hh>
+#include <G4TouchableHistory.hh>
+#include <G4RunManager.hh>
+#include <G4OpticalPhoton.hh>
+
+using namespace std;
+
+G4MPMTAction::G4MPMTAction(const G4String& name, const G4int dId, const G4int oId, Event& theEvent) :
+    G4VSensitiveDetector(name),
+    fEvent(theEvent),
+    fDetectorId(dId),
+    fOptDeviceId(oId)
+
+  { 
+    
+    cout << "[INFO] G4Models::G4MPMTAction: Registering PMT " << name << endl;
+    SimData& simData = fEvent.GetSimData();
+    DetectorSimData& detSimData = simData.GetDetectorSimData(fDetectorId);
+    detSimData.MakeSiPMSimData(fOptDeviceId);
+    SiPMSimData& OptDeviceSimData = detSimData.GetSiPMSimData(fOptDeviceId);
+
+    fPETimeDistribution = OptDeviceSimData.PETimeDistributionRange();
+
+  }
+
+void
+G4MPMTAction::Initialize(G4HCofThisEvent* const /*hce*/)
+{
+  
+  fPETime = new std::vector<double>();
+
+}
+
+void
+G4MPMTAction::EndOfEvent(G4HCofThisEvent* const /*hce*/)
+{
+  fEvent.GetSimData().GetDetectorSimData(fDetectorId).GetSiPMSimData(fOptDeviceId).AddPETimeDistribution(fPETime);
+  //fPETimeDistribution->push_back(fPETime);
+
+}
+
+G4bool
+G4MPMTAction::ProcessHits(G4Step* const step, G4TouchableHistory* const /*rOHist*/)
+{
+  // reject particle in case it is not a photon
+
+  if (step->GetTrack()->GetDefinition() != G4OpticalPhoton::OpticalPhotonDefinition())
+    return true;
+
+  
+  
+}
+  
+
+
---
+++ src/G4Models/G4MPMTAction.h
@@ -0,0 +1,33 @@
+#ifndef G4MPMTAction_h 
+#define G4MPMTAction_h 1
+
+#include "Event.h"
+#include <G4VSensitiveDetector.hh>
+
+
+class G4Step;
+class G4HCofThisEvent;
+class G4TouchableHistory;
+
+class G4MPMTAction : public G4VSensitiveDetector {
+  public:
+    G4MPMTAction(const G4String& name, const G4int dId, const G4int oId, Event& theEvent);
+    virtual ~G4MPMTAction() { }
+
+    virtual void Initialize(G4HCofThisEvent* const /*hce*/) override;
+    virtual G4bool ProcessHits(G4Step* const step, G4TouchableHistory* const rOhist) override;
+    virtual void EndOfEvent(G4HCofThisEvent* const /*hce*/) override;
+
+  private:
+    
+    Event& fEvent;
+
+    int fDetectorId = 0;
+    int fOptDeviceId = 0;
+
+    std::vector<double>* fPETime;
+    std::vector<std::vector<double>*>* fPETimeDistribution;
+};
+
+
+#endif
