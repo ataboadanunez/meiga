684325fea7d79f55f008b83aa398b623b0920b43
Alvaro Taboada <alvaro.taboada.nunez@gmail.com>
2022-02-18 19:18:10 -0300

    WCD simulator app finished. for now, dumps into ascii files number of PE for each particle component

--- src/Applications/G4WCDSimulator/G4WCDSimulator.cc
+++ src/Applications/G4WCDSimulator/G4WCDSimulator.cc
@@ -1,9 +1,9 @@
 /* 
-	
-	A Meiga application for simulating Water-Cherenkov Detectors
+  
+  A Meiga application for simulating Water-Cherenkov Detectors
 
-	author: alvaro taboada
-	date: 9 Feb 2022
+  author: alvaro taboada
+  date: 9 Feb 2022
 
  */
 
@@ -57,11 +57,11 @@ G4WCDSimulator::G4WCDSimulator()
 
 namespace 
 {
-	void ProgramUsage() 
-	{
-		cerr << " Program Usage: " << endl;
-		cerr << " ./exeMeigaApp [ -c ConfigFile.json ] " << endl;
-	}
+  void ProgramUsage() 
+  {
+    cerr << " Program Usage: " << endl;
+    cerr << " ./exeMeigaApp [ -c ConfigFile.json ] " << endl;
+  }
 
 }
 
@@ -69,31 +69,41 @@ namespace
 int main(int argc, char** argv) 
 {
 
-	if (argc < 3) {
-		ProgramUsage();
-		throw invalid_argument("A config file is needed!");
-	}
+  if (argc < 3) {
+    ProgramUsage();
+    throw invalid_argument("A config file is needed!");
+  }
 
-	for (int i=1; i<argc; i=i+2) {
-		string sarg(argv[i]);
-		if (sarg == "-c")
-			fCfgFile = argv[i+1];
-	}
+  for (int i=1; i<argc; i=i+2) {
+    string sarg(argv[i]);
+    if (sarg == "-c")
+      fCfgFile = argv[i+1];
+  }
 
-	Event theEvent;
-	fG4WCDSimulator = new G4WCDSimulator();
-	fG4WCDSimulator->Initialize(theEvent, fCfgFile);
-	fG4WCDSimulator->RunSimulation(theEvent);
-	/*************************************************
-		
-		Geant4 simulation ended here!
-		What happens next is up to you =)
+  // for program time calculation
+  time_t start, end;
+  time(&start);
 
-	**************************************************/
-	fG4WCDSimulator->WriteEventInfo(theEvent);
-	
-	
-	return 0;
+  Event theEvent;
+  fG4WCDSimulator = new G4WCDSimulator();
+  fG4WCDSimulator->Initialize(theEvent, fCfgFile);
+  fG4WCDSimulator->RunSimulation(theEvent);
+  /*************************************************
+    
+    Geant4 simulation ended here!
+    What happens next is up to you =)
+
+  **************************************************/
+  fG4WCDSimulator->WriteEventInfo(theEvent);
+  time(&end);
+
+  // Calculating total time taken by the program.
+    double time_taken = double(end - start);
+    cout << "Time taken by program is : " << fixed
+         << time_taken << setprecision(5);
+    cout << " sec " << endl;
+  
+  return 0;
 
 }
 
@@ -101,33 +111,33 @@ void
 G4WCDSimulator::Initialize(Event& theEvent, string fileName)
 {
 
-	cout << "... Initialize ..." << endl;
-	// set value of flags according to cfg file
-	// reading cfg file using boost
-	// eventually as input of executable main(char** fConfig)
-	
-	fInputFile.clear();
-	fOutputFile.clear();
+  cout << "... Initialize ..." << endl;
+  // set value of flags according to cfg file
+  // reading cfg file using boost
+  // eventually as input of executable main(char** fConfig)
+  
+  fInputFile.clear();
+  fOutputFile.clear();
 
-	ptree root;
-	read_json(fileName, root);
+  ptree root;
+  read_json(fileName, root);
 
-	fInputFile = root.get<string>("InputFile");
-	fOutputFile = root.get<string>("OutputFile");
-	fSimulationMode = root.get<string>("SimulationMode");
-	fDetectorList = root.get<string>("DetectorList");
-	fGeoVisOn = root.get<bool>("GeoVisOn");
-	fTrajVisOn = root.get<bool>("TrajVisOn");
-	fVerbosity = root.get<int>("Verbosity");
-	fRenderFile = root.get<string>("RenderFile");
-	fPhysicsName = root.get<string>("PhysicsName");
-	
-	// Creates event from file reader
-	theEvent = ReadParticleFile::EventFileReader(fInputFile);
+  fInputFile = root.get<string>("InputFile");
+  fOutputFile = root.get<string>("OutputFile");
+  fSimulationMode = root.get<string>("SimulationMode");
+  fDetectorList = root.get<string>("DetectorList");
+  fGeoVisOn = root.get<bool>("GeoVisOn");
+  fTrajVisOn = root.get<bool>("TrajVisOn");
+  fVerbosity = root.get<int>("Verbosity");
+  fRenderFile = root.get<string>("RenderFile");
+  fPhysicsName = root.get<string>("PhysicsName");
+  
+  // Creates event from file reader
+  theEvent = ReadParticleFile::EventFileReader(fInputFile);
 
-	// Read Detector Configuration
-	ConfigManager::ReadDetectorList(fDetectorList, theEvent);
-	
+  // Read Detector Configuration
+  ConfigManager::ReadDetectorList(fDetectorList, theEvent);
+  
 }            
 
 
@@ -135,129 +145,129 @@ bool
 G4WCDSimulator::RunSimulation(Event& theEvent)
 {
 
-	SimData& simData = theEvent.GetSimData();
-	simData.SetSimulationMode(fSimulationMode);
-	cout << "[INFO] G4WCDSimulator::RunSimulation: Simulation mode selected = " << fSimulationMode << endl;
-	const unsigned int NumberOfParticles = simData.GetTotalNumberOfParticles();
+  SimData& simData = theEvent.GetSimData();
+  simData.SetSimulationMode(fSimulationMode);
+  cout << "[INFO] G4WCDSimulator::RunSimulation: Simulation mode selected = " << fSimulationMode << endl;
+  const unsigned int NumberOfParticles = simData.GetTotalNumberOfParticles();
 
-	cout << "[INFO] G4WCDSimulator::RunSimulation: Total number of particles in file = " << NumberOfParticles << endl;
-	
-	if (!NumberOfParticles) {
-		cerr << "[ERROR] G4WCDSimulator::RunSimulation: No Particles in the Event! Exiting." << endl;
-		return false;
-	}
-	
-	/***************
+  cout << "[INFO] G4WCDSimulator::RunSimulation: Total number of particles in file = " << NumberOfParticles << endl;
+  
+  if (!NumberOfParticles) {
+    cerr << "[ERROR] G4WCDSimulator::RunSimulation: No Particles in the Event! Exiting." << endl;
+    return false;
+  }
+  
+  /***************
 
-		Geant4 Setup    
+    Geant4 Setup    
 
-	*****************/
+  *****************/
 
-	G4long myseed = time(NULL);
-	G4Random::setTheEngine(new CLHEP::RanecuEngine);
-	G4Random::setTheSeed(myseed);
+  G4long myseed = time(NULL);
+  G4Random::setTheEngine(new CLHEP::RanecuEngine);
+  G4Random::setTheSeed(myseed);
 
-	G4VisManager* fVisManager = nullptr;
-	
-	// construct the default run manager
-	auto fRunManager = G4RunManagerFactory::CreateRunManager();
+  G4VisManager* fVisManager = nullptr;
+  
+  // construct the default run manager
+  auto fRunManager = G4RunManagerFactory::CreateRunManager();
 
-	// set mandatory initialization classes
-	auto fDetConstruction = new G4WCDConstruction(theEvent);
-	fRunManager->SetUserInitialization(fDetConstruction);
-	
-	//G4VModularPhysicsList* physicsList = new FTFP_BERT;
-	//fRunManager->SetUserInitialization(physicsList);
-	fRunManager->SetUserInitialization(new G4MPhysicsList(fPhysicsName));
+  // set mandatory initialization classes
+  auto fDetConstruction = new G4WCDConstruction(theEvent);
+  fRunManager->SetUserInitialization(fDetConstruction);
+  
+  //G4VModularPhysicsList* physicsList = new FTFP_BERT;
+  //fRunManager->SetUserInitialization(physicsList);
+  fRunManager->SetUserInitialization(new G4MPhysicsList(fPhysicsName));
 
-	G4WCDPrimaryGeneratorAction *fPrimaryGenerator = new G4WCDPrimaryGeneratorAction();
-	fRunManager->SetUserAction(fPrimaryGenerator);
-	
-	G4WCDStackingAction *fStackingAction = new G4WCDStackingAction(theEvent);
-	fRunManager->SetUserAction(fStackingAction);
+  G4WCDPrimaryGeneratorAction *fPrimaryGenerator = new G4WCDPrimaryGeneratorAction();
+  fRunManager->SetUserAction(fPrimaryGenerator);
+  
+  G4WCDStackingAction *fStackingAction = new G4WCDStackingAction(theEvent);
+  fRunManager->SetUserAction(fStackingAction);
 
-	G4WCDRunAction *fRunAction = new G4WCDRunAction();
-	fRunManager->SetUserAction(fRunAction);
-	
-	G4WCDEventAction *fEventAction = new G4WCDEventAction();
-	fRunManager->SetUserAction(fEventAction);
+  G4WCDRunAction *fRunAction = new G4WCDRunAction();
+  fRunManager->SetUserAction(fRunAction);
+  
+  G4WCDEventAction *fEventAction = new G4WCDEventAction();
+  fRunManager->SetUserAction(fEventAction);
 
-	fRunManager->SetUserAction(new G4WCDTrackingAction());
+  fRunManager->SetUserAction(new G4WCDTrackingAction());
 
-	G4WCDSteppingAction *fSteppingAction = new G4WCDSteppingAction(fDetConstruction, fEventAction, theEvent);
-	fRunManager->SetUserAction(fSteppingAction);
-	
-	// initialize G4 kernel
-	fRunManager->Initialize();
+  G4WCDSteppingAction *fSteppingAction = new G4WCDSteppingAction(fDetConstruction, fEventAction, theEvent);
+  fRunManager->SetUserAction(fSteppingAction);
+  
+  // initialize G4 kernel
+  fRunManager->Initialize();
 
-	// initialize visualization
-	if ((fGeoVisOn || fTrajVisOn) && !fVisManager)
-		fVisManager = new G4VisExecutive;
+  // initialize visualization
+  if ((fGeoVisOn || fTrajVisOn) && !fVisManager)
+    fVisManager = new G4VisExecutive;
 
-	// get the pointer to the UI manager and set verbosities
-	G4UImanager* fUImanager = G4UImanager::GetUIpointer();
-	switch (fVerbosity) {
-		case 1:
-			fUImanager->ApplyCommand("/run/verbose 1");
-			fUImanager->ApplyCommand("/event/verbose 0");
-			fUImanager->ApplyCommand("/tracking/verbose 0");
-			break;
-		case 2:
-			fUImanager->ApplyCommand("/run/verbose 1");
-			fUImanager->ApplyCommand("/event/verbose 1");
-			fUImanager->ApplyCommand("/tracking/verbose 0");
-			break;
-		case 3:
-			fUImanager->ApplyCommand("/run/verbose 1");
-			fUImanager->ApplyCommand("/event/verbose 1");
-			fUImanager->ApplyCommand("/tracking/verbose 1");
-			break;
-		default:
-			fUImanager->ApplyCommand("/run/verbose 0");
-			fUImanager->ApplyCommand("/event/verbose 0");
-			fUImanager->ApplyCommand("/tracking/verbose 0");
-		}
-	
-	if (fGeoVisOn || fTrajVisOn) {
-		fVisManager->Initialize();
-		fUImanager->ApplyCommand(("/vis/open " + fRenderFile).c_str());
-		fUImanager->ApplyCommand("/vis/scene/create");
-		fUImanager->ApplyCommand("/vis/sceneHandler/attach");
-		fUImanager->ApplyCommand("/vis/scene/add/volume");
-		fUImanager->ApplyCommand("/vis/scene/add/axes 0 0 0 1 m");
-		fUImanager->ApplyCommand("/vis/viewer/set/viewpointThetaPhi 0. 0.");
-		fUImanager->ApplyCommand("/vis/viewer/set/targetPoint 0 0 0");
-		fUImanager->ApplyCommand("/vis/viewer/zoom 1");
-		fUImanager->ApplyCommand("/vis/viewero/set/style/wireframe");
-		fUImanager->ApplyCommand("/vis/drawVolume");
-		fUImanager->ApplyCommand("/vis/scene/notifyHandlers");
-		fUImanager->ApplyCommand("/vis/viewer/update");
+  // get the pointer to the UI manager and set verbosities
+  G4UImanager* fUImanager = G4UImanager::GetUIpointer();
+  switch (fVerbosity) {
+    case 1:
+      fUImanager->ApplyCommand("/run/verbose 1");
+      fUImanager->ApplyCommand("/event/verbose 0");
+      fUImanager->ApplyCommand("/tracking/verbose 0");
+      break;
+    case 2:
+      fUImanager->ApplyCommand("/run/verbose 1");
+      fUImanager->ApplyCommand("/event/verbose 1");
+      fUImanager->ApplyCommand("/tracking/verbose 0");
+      break;
+    case 3:
+      fUImanager->ApplyCommand("/run/verbose 1");
+      fUImanager->ApplyCommand("/event/verbose 1");
+      fUImanager->ApplyCommand("/tracking/verbose 1");
+      break;
+    default:
+      fUImanager->ApplyCommand("/run/verbose 0");
+      fUImanager->ApplyCommand("/event/verbose 0");
+      fUImanager->ApplyCommand("/tracking/verbose 0");
+    }
+  
+  if (fGeoVisOn || fTrajVisOn) {
+    fVisManager->Initialize();
+    fUImanager->ApplyCommand(("/vis/open " + fRenderFile).c_str());
+    fUImanager->ApplyCommand("/vis/scene/create");
+    fUImanager->ApplyCommand("/vis/sceneHandler/attach");
+    fUImanager->ApplyCommand("/vis/scene/add/volume");
+    fUImanager->ApplyCommand("/vis/scene/add/axes 0 0 0 1 m");
+    fUImanager->ApplyCommand("/vis/viewer/set/viewpointThetaPhi 0. 0.");
+    fUImanager->ApplyCommand("/vis/viewer/set/targetPoint 0 0 0");
+    fUImanager->ApplyCommand("/vis/viewer/zoom 1");
+    fUImanager->ApplyCommand("/vis/viewero/set/style/wireframe");
+    fUImanager->ApplyCommand("/vis/drawVolume");
+    fUImanager->ApplyCommand("/vis/scene/notifyHandlers");
+    fUImanager->ApplyCommand("/vis/viewer/update");
 
-	}
+  }
 
-	if (fTrajVisOn) {
-			fUImanager->ApplyCommand("/tracking/storeTrajectory 1");
-			fUImanager->ApplyCommand("/vis/scene/add/trajectories");
-	}
-	
+  if (fTrajVisOn) {
+      fUImanager->ApplyCommand("/tracking/storeTrajectory 1");
+      fUImanager->ApplyCommand("/vis/scene/add/trajectories");
+  }
+  
 
-	int nParticle = 0;
-	// loop over particle vector
-	for (auto it = simData.GetParticleVector().begin(); it != simData.GetParticleVector().end(); ++it) {
-		G4WCDSimulator::currentParticle = *it;
-		simData.SetCurrentParticle(*it);
-		// Run simulation
-		fRunManager->BeamOn(1);
-		nParticle+=1;
+  int nParticle = 0;
+  // loop over particle vector
+  for (auto it = simData.GetParticleVector().begin(); it != simData.GetParticleVector().end(); ++it) {
+    G4WCDSimulator::currentParticle = *it;
+    simData.SetCurrentParticle(*it);
+    // Run simulation
+    fRunManager->BeamOn(1);
+    nParticle+=1;
 
-		//cout << "Simulated " << nParticle << " particle(s) out of " << NumberOfParticles << endl;
-	}
+    //cout << "Simulated " << nParticle << " particle(s) out of " << NumberOfParticles << endl;
+  }
 
 
-	delete fVisManager;
-	delete fRunManager;
+  delete fVisManager;
+  delete fRunManager;
 
-	return true;
+  return true;
 
 }
 
@@ -265,50 +275,88 @@ G4WCDSimulator::RunSimulation(Event& theEvent)
 void
 G4WCDSimulator::WriteEventInfo(Event& theEvent)
 {
-	cout << "... WriteEventInfo ..." << endl;
+  cout << "... WriteEventInfo ..." << endl;
 
-	// for accessing Simulated Data at Detector/Event level
-	SimData& simData = theEvent.GetSimData();
-	
-	// loop over detector range
-	for (auto detIt = theEvent.DetectorRange().begin(); detIt != theEvent.DetectorRange().end(); detIt++) {
+  // for accessing Simulated Data at Detector/Event level
+  SimData& simData = theEvent.GetSimData();
+  
+  // loop over detector range
+  for (auto detIt = theEvent.DetectorRange().begin(); detIt != theEvent.DetectorRange().end(); detIt++) {
 
-		auto& currDet = detIt->second;
-		int detId = currDet.GetId();
+    auto& currDet = detIt->second;
+    int detId = currDet.GetId();
 
-		DetectorSimData& detSimData = simData.GetDetectorSimData(detId);
-		// get number of optical devices in the detector
-		int nOptDev = currDet.GetNOptDevice();
-		cout << "[INFO] G4WCDSimulator::WriteEventInfo: Accessing data of detector " << detId << " with " << nOptDev << " optical devices" << endl;
+    DetectorSimData& detSimData = simData.GetDetectorSimData(detId);
+    // get number of optical devices in the detector
+    int nOptDev = currDet.GetNOptDevice();
+    cout << "[INFO] G4WCDSimulator::WriteEventInfo: Accessing data of detector " << detId << " with " << nOptDev << " optical devices" << endl;
 
-		// loop over optical devices
-		for (auto odIt = currDet.OptDeviceRange().begin(); odIt != currDet.OptDeviceRange().end(); odIt++) {
-			auto& currOd = odIt->second;
-			int odId = currOd.GetId();
+    // loop over optical devices
+    for (auto odIt = currDet.OptDeviceRange().begin(); odIt != currDet.OptDeviceRange().end(); odIt++) {
+      auto& currOd = odIt->second;
+      int odId = currOd.GetId();
 
-			OptDeviceSimData& odSimData = detSimData.GetOptDeviceSimData(odId);
-			cout << "[INFO] G4WCDSimulator::WriteEventInfo: Accessing signal of " << currOd.GetName() << " " << odId << " from Detector " << detId << endl;
+      OptDeviceSimData& odSimData = detSimData.GetOptDeviceSimData(odId);
+      cout << "[INFO] G4WCDSimulator::WriteEventInfo: Accessing signal of " << currOd.GetName() << " " << odId << " from Detector " << detId << endl;
 
-			// checking signal at optical devices
-			const auto *peTimeDistributionRange = odSimData.PETimeDistributionRange();
-			if (!peTimeDistributionRange) {
-				cerr << "No Time for this channel!" << endl;
-				continue;
-			}
-
-			for (auto peTime = peTimeDistributionRange->begin(); peTime != peTimeDistributionRange->end(); ++peTime) {
-
-				size_t npe = (*peTime)->size();
-				cout << "Number of photo-electrons = " << npe << endl;
-				// write pulses to output file
-
-			}
+      // checking signal at optical devices
+      const auto *peTimeDistributionRange = odSimData.PETimeDistributionRange();
+      if (!peTimeDistributionRange) {
+        cerr << "No Time for this channel!" << endl;
+        continue;
+      }
 
 
-		}
+      // // accessing signals (photo-electrons)
+      // cout << "[INFO] G4WCDSimulator::WriteEventInfo: Number of photo-electrons at " << currOd.GetName() << endl; 
+      // for (auto peTime = peTimeDistributionRange->begin(); peTime != peTimeDistributionRange->end(); ++peTime) {
+
+      //   size_t npe = (*peTime)->size();
+      //   cout << npe << " ";
+      //   // write pulses to output file
+
+      // }
+
+      cout << endl;
+      // accessing signals for different particle components
+
+      for (int compIt = Particle::eElectromagnetic; compIt < Particle::eEnd; compIt++) {
+        
+        Particle::Component particleComponent = static_cast<Particle::Component>(compIt);
+        cout << "[INFO] G4WCDSimulator::WriteEventInfo: Accessing signals of particle component " << particleComponent << endl;
+        try 
+        { 
+          const auto peTimeDistributionRangeComp = odSimData.PETimeDistributionRange(particleComponent);
+          if (!peTimeDistributionRange) {
+            cerr << "No time distribution for particle component " << particleComponent << endl;
+            continue;
+          }
+
+          ofstream* fPEFile = new ofstream();
+          fPEFile->open("photoelectrons_"+std::to_string(detId)+"_PMT_"+std::to_string(odId)+std::to_string(particleComponent)+".dat");
+          for (auto peTime = peTimeDistributionRangeComp.begin(); peTime != peTimeDistributionRangeComp.end(); ++peTime) {
+            size_t npe = peTime->size();
+            (*fPEFile) << npe << " ";
+          }
+          
+          (*fPEFile) << endl;
+          cout << "[INFO] G4WCDSimulator::WriteEventInfo: Signals coppied!" << endl;
+          fPEFile->close();
+        }
+        catch (std::out_of_range &e)
+        {
+          cerr << "No time distribution for particle component " << particleComponent << "! (Exception: " << e.what() << " )." << endl;
+        }
+        
+        
+      
+      }
 
 
-	}
+    }
+
+
+  }
 
 
 }
