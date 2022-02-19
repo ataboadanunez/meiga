f606e36f849f4c2e98fd696adc1c8c409b07376b
alvaro <alvaro.taboada.nunez@gmail.com>
2022-02-19 16:56:34 -0300

    cleaning

--- src/Applications/G4WCDSimulator/G4WCDConstruction.cc
+++ src/Applications/G4WCDSimulator/G4WCDConstruction.cc
@@ -2,12 +2,9 @@
 #include <iostream>
 
 #include "G4WCDConstruction.h"
-// include detector models (should be in separate header)
 #include "Materials.h"
-#include "Musaic.h"
-#include "Mudulus.h"
-//#include "RCDetector.h"
 
+#include "G4SDManager.hh"
 #include "G4UnitsTable.hh"
 
 using namespace std;
@@ -43,39 +40,37 @@ G4WCDConstruction::CreateWorld()
 
 }
 
-// void
-// G4WCDConstruction::CreateGround()
-// {
-// 	solidGround = new G4Box("Ground", fGroundSizeX/2, fGroundSizeY/2, fGroundSizeZ/2);
-// 	G4VisAttributes brown(G4Colour::Brown());
-// 	logicGround = new G4LogicalVolume(solidGround, Materials().StdRock, "Ground");
-// 	logicGround->SetVisAttributes(brown);
-// 	physGround  =  new G4PVPlacement(nullptr, G4ThreeVector(0, 0, -fWorldSizeZ/2 + fGroundSizeZ/2), logicGround, "Ground", logicWorld, false, 0, fCheckOverlaps);
-// }
+void
+G4WCDConstruction::CreateGround()
+{
+	solidGround = new G4Box("Ground", fGroundSizeX/2, fGroundSizeY/2, fGroundSizeZ/2);
+	G4VisAttributes brown(G4Colour::Brown());
+	logicGround = new G4LogicalVolume(solidGround, Materials().StdRock, "Ground");
+	logicGround->SetVisAttributes(brown);
+	physGround  =  new G4PVPlacement(nullptr, G4ThreeVector(0, 0, -fWorldSizeZ/2 + fGroundSizeZ/2), logicGround, "Ground", logicWorld, false, 0, fCheckOverlaps);
+}
 
 
 void
 G4WCDConstruction::PlaceDetector(Event& theEvent)
 {
 	
-	//Detector& detector = theEvent.GetDetector();
 	int nDetectors = theEvent.GetNDetectors();
 	// loop in detector vector
 	for (auto detIt = theEvent.DetectorRange().begin(); detIt != theEvent.DetectorRange().end(); detIt++) {
 		auto& currentDet = detIt->second;
-		cout << "[DEBUG] G4WCDConstruction: Detector Type = " << currentDet.GetType() << endl;
-			BuildDetector(logicWorld, currentDet, theEvent, true);
+		BuildDetector(logicWorld, currentDet, theEvent, true);
 
 	}
 
 }
+
 
 G4VPhysicalVolume* 
 G4WCDConstruction::Construct() 
 {
 
 	if (!physWorld) {
-		//CreateElements();
 		return CreateDetector();
 	}
 	return physWorld;
