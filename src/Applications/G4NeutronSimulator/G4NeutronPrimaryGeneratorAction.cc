0a872b55f5b0e6acff2880cb1b9a0d8d52a06ced
alvaro.taboada <alvaro.taboada.nunez@gmail.com>
2022-10-06 13:58:13 -0300

    injection position set from configuration

--- src/Applications/G4NeutronSimulator/G4NeutronPrimaryGeneratorAction.cc
+++ src/Applications/G4NeutronSimulator/G4NeutronPrimaryGeneratorAction.cc
@@ -22,90 +22,95 @@ using CLHEP::RandFlat;
 using namespace std;
 
 G4NeutronPrimaryGeneratorAction::G4NeutronPrimaryGeneratorAction(Event& theEvent) : 
-  G4VUserPrimaryGeneratorAction(),
-  fParticleGun(new G4ParticleGun(1)),
-  fEvent(theEvent)
-  
+	G4VUserPrimaryGeneratorAction(),
+	fParticleGun(new G4ParticleGun(1)),
+	fEvent(theEvent)
+	
 {  
 
 	fDetectorConstructor = static_cast<const G4NeutronDetectorConstruction*> (G4RunManager::GetRunManager()->GetUserDetectorConstruction());
-  
+	
 
 }
 
 G4NeutronPrimaryGeneratorAction::~G4NeutronPrimaryGeneratorAction()
 {
-  delete fParticleGun;
+	delete fParticleGun;
 }
 
 
 void 
 G4NeutronPrimaryGeneratorAction::GeneratePrimaries(G4Event* event)
 {
-  
-  // get particle information
-  Particle& currParticle = G4NeutronSimulator::currentParticle;
-  G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
+	
+	// get particle information
+	Particle& currParticle = G4NeutronSimulator::currentParticle;
+	G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
 
-  unsigned int currParticleID = currParticle.GetParticleId();
-  int fParticleId = Corsika::CorsikaToPDG(currParticleID);
-  G4ParticleDefinition* particleDef = particleTable->FindParticle(fParticleId);
+	unsigned int currParticleID = currParticle.GetParticleId();
+	int fParticleId = Corsika::CorsikaToPDG(currParticleID);
+	G4ParticleDefinition* particleDef = particleTable->FindParticle(fParticleId);
 
-  if (!particleDef) {
-      cout << "[WARNING] G4NeutronPrimaryGeneratorAction::GeneratePrimaries: Undefined particle with Corsika ID: " << currParticleID << endl;
-      return;
-    }
-  
-  double maxHeight = fEvent.GetMaximumHeight();
+	if (!particleDef) {
+			cout << "[WARNING] G4NeutronPrimaryGeneratorAction::GeneratePrimaries: Undefined particle with Corsika ID: " << currParticleID << endl;
+			return;
+		}
+	
+	double fKineticEnergy = currParticle.GetKineticEnergy();
 
+	const std::vector<double> particleMomentumDirection = currParticle.GetDirection();
+	double fPx = particleMomentumDirection.at(0);
+	double fPy = particleMomentumDirection.at(1);
+	double fPz = particleMomentumDirection.at(2);
+	
+	fParticleGun->SetParticleDefinition(particleDef);
 
-  double mass = currParticle.GetMass();
-  double fMomentum = currParticle.GetMomentum();
-  double fKineticEnergy = currParticle.GetKineticEnergy();
-  auto& particlePosition = currParticle.GetParticlePosition();
+	G4double px2 = fPx*fPx;
+	G4double py2 = fPy*fPy;
+	G4double pz2 = fPz*fPz;
 
-  double posX = particlePosition.at(0);
-  double posY = particlePosition.at(1);
-  double posZ = particlePosition.at(2);
+	G4double particleMomentum = sqrt(px2 + py2 + pz2);
+	G4double particleZenith = acos(fPz / particleMomentum);
+	G4double particleAzimut = atan2(-fPy, -fPx);
 
-  const std::vector<double> particleMomentumDirection = currParticle.GetDirection();
- 	double fPx = particleMomentumDirection.at(0);
-  double fPy = particleMomentumDirection.at(1);
-  double fPz = particleMomentumDirection.at(2);
-  
-  fParticleGun->SetParticleDefinition(particleDef);
+	currParticle.SetAzimuth(particleAzimut);
+	currParticle.SetZenith(particleZenith);
 
-  G4double px2 = fPx*fPx;
-  G4double py2 = fPy*fPy;
-  G4double pz2 = fPz*fPz;
+	/* 
+		PARTICLE INJECTION
+		use injection position from SimData (through DetectorList)
+	*/
+	// get detector position as reference for injection position
+	SimData &simData = fEvent.GetSimData();
+	const double injHeight = simData.GetInjectionHeight();
+	const double injRadius = simData.GetInjectionRadius();
+	if (!(injHeight || injRadius)) 
+		cout << "[WARNING] G4NeutronPrimaryGeneratorAction::GeneratePrimaries: Using default values for injection area!" << endl;
 
-  G4double particleMomentum = sqrt(px2 + py2 + pz2);
-  G4double particleZenith = acos(fPz / particleMomentum);
-  G4double particleAzimut = atan2(-fPy, -fPx);
+	cout << "[INFO] G4NeutronPrimaryGeneratorAction::GeneratePrimaries: Injecting particles over circumference of Radius = " << injRadius / CLHEP::m << " at Height = " << injHeight / CLHEP::m << endl;
 
-  currParticle.SetAzimuth(particleAzimut);
-  currParticle.SetZenith(particleZenith);
+	double circArea = CLHEP::pi * pow(injRadius, 2);
+	cout << "[INFO] G4NeutronPrimaryGeneratorAction::GeneratePrimaries: Injection area A = " << circArea / (pow(CLHEP::m, 2)) << " m2, at z = " << injHeight / CLHEP::m << " m." << endl;
+	
+	// x and y coordinates randomly uniformed over a circumference of radius injRadius.
+	double rand1 = RandFlat::shoot(0., 1.);
+	double rand2 = RandFlat::shoot(0., CLHEP::twopi);
+	double r = injRadius * sqrt(rand1);
+	// compute injection position coordinates
+	double x0 = r*cos(rand2);
+	G4double y0 = r*sin(rand2);
+	G4double z0 = injHeight;
 
-  // check injection according to detector dimensions
-  const G4double injRadius = 0.5*m;
-  const G4double injHeight = maxHeight;  
-  const G4double rand = RandFlat::shoot(0., 1.);
-  const G4double r = injRadius*sqrt(rand);
-  const G4double phi = RandFlat::shoot(0., CLHEP::twopi);
+	cout << "[INFO] G4NeutronPrimaryGeneratorAction::GeneratePrimaries: Injecting particle " << particleDef->GetParticleName() << " with Corsika ID: " << currParticleID << endl;
+	cout << "[INFO] G4NeutronPrimaryGeneratorAction::GeneratePrimaries: Particle position = (" << x0 / CLHEP::m << ", " << y0 / CLHEP::m << ", " << z0 / CLHEP::m << ") m " << endl;
+	cout << "[INFO] G4NeutronPrimaryGeneratorAction::GeneratePrimaries: Particle energy = " << fKineticEnergy / MeV << " MeV" << endl;
+	
+	const std::vector<double> injectionPosition = {x0, y0, z0};
+	currParticle.SetInjectionPosition(injectionPosition);
+	
+	fParticleGun->SetParticleMomentumDirection(G4ThreeVector(fPx, fPy, -1*fPz));
+	fParticleGun->SetParticlePosition(G4ThreeVector(x0, y0, z0));
+	fParticleGun->SetParticleEnergy(fKineticEnergy);
 
-  G4double x0 = r*cos(phi);
-  G4double y0 = r*sin(phi);
-  G4double z0 = injHeight;
-
-  std::cout << "[INFO] G4NeutronPrimaryGeneratorAction::GeneratePrimaries: Injection Position = (" << x0 / cm << ", " << y0 / cm << ", " << z0 / cm << ") cm " << std::endl;
-  std::cout << "[INFO] G4NeutronPrimaryGeneratorAction::GeneratePrimaries: Particle energy = " << fKineticEnergy / MeV << " MeV" << std::endl;
-  
-  const std::vector<double> injectionPosition = {x0, y0, z0};
-  currParticle.SetInjectionPosition(injectionPosition);
-  
-  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(fPx, fPy, -1*fPz));
-  fParticleGun->SetParticlePosition(G4ThreeVector(x0, y0, z0));
-  fParticleGun->SetParticleEnergy(fKineticEnergy);
-
-  fParticleGun->GeneratePrimaryVertex(event);
+	fParticleGun->GeneratePrimaryVertex(event);
 }
